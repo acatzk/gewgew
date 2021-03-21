8daf910e7e55e7226b3796414db1431f19fbdaa3
Joshua Galit <joshuaimalay@gmail.com>
2021-03-21 16:03:56 +0800

    feat: loading screen icon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,5 @@
 import '~/styles/tailwind.css'
 
-function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
