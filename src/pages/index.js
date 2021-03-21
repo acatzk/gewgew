777fc6c30f63f0690de4c491282b3981cf76b288
Joshua Galit <joshuaimalay@gmail.com>
2021-03-21 15:36:23 +0800

    init

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Social Network</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className="text-2xl font-semibold text-blue-500">Welcome New Project</h1>
+    </>
+  )
+}
