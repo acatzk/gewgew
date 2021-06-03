b8982ecbb41cedf55712485aa2922e8fd4c463a8
acatzk <joshuaimalay@gmail.com>
2021-06-03 15:44:59 +0800

    feat: tailwindcss jit at core mode

---
+++ src/pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
