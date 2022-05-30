053387413debe92e17cf75ff626705fdc703108b
acatzk <joshuaimalay@gmail.com>
2022-05-30 13:39:12 +0800

    feat: setup nhost-client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import '~/styles/tailwind.css'
 import type { AppProps } from 'next/app'
+import { nhost } from '~/lib/nhost-client'
+import { NhostNextProvider } from '@nhost/nextjs'
+import { NhostApolloProvider } from '@nhost/react-apollo'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <NhostNextProvider nhost={nhost} initial={pageProps.nhostSession}>
+      <NhostApolloProvider nhost={nhost}>
+        <Component {...pageProps} />
+      </NhostApolloProvider>
+    </NhostNextProvider>
+  )
 }
 
 export default MyApp
