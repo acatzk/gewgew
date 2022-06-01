868da6a9b94532963c648c0290cd2f92ab783907
acatzk <joshuaimalay@gmail.com>
2022-06-01 18:50:15 +0800

    wip: side navbar scroll layout (#25)

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
+import Image from 'next/image'
+import { BiSearch } from 'react-icons/bi'
+import { MdVerified } from 'react-icons/md'
 import { classNames } from '~/utils/classNames'
-import { suggedtedItems, SuggedtedProps } from '~/utils/object-items'
+import {
+  suggedtedItems,
+  SuggedtedProps,
+  dummyAccounts,
+  DummyAccountProps,
+} from '~/utils/object-items'
 
 type Props = {
   children: React.ReactNode
@@ -10,11 +18,11 @@ const HomeLayout: React.FC<Props> = (props) => {
   const { children } = props
   return (
     <React.Fragment>
-      <div className="relative w-1/4 flex-grow hidden lg:block border-r border-gray-700 min-h-screen">
+      <section className="relative w-1/4 flex-grow hidden lg:block border-r border-gray-700 min-h-screen overflow-hidden">
         <div className="flex flex-col px-3 py-5 overflow-y-aut">
-          <h1 className="text-2xl font-bold">Gewgew</h1>
+          <h1 className="text-2xl font-bold px-2">Gewgew</h1>
           <nav className="mt-4 flex flex-col">
-            <h2 className="font-bold text-sm">Suggested</h2>
+            <h2 className="font-bold text-sm px-2">Suggested</h2>
             <ul className="mt-2">
               {suggedtedItems.map((list, i) => (
                 <SuggestedList key={i} {...list} />
@@ -27,11 +35,52 @@ const HomeLayout: React.FC<Props> = (props) => {
             All right reserved &copy; 2022
           </p>
         </div>
-      </div>
-      <div className="relative w-1/2 flex-grow lg:flex-none border-r border-gray-700">
+      </section>
+      <main className="relative w-1/2 flex-grow lg:flex-none border-r border-gray-700">
         {children}
-      </div>
-      <div className="relative w-1/4 flex-grow hidden md:block"></div>
+      </main>
+      <section className="relative w-1/4 flex-grow hidden md:block min-h-screen overflow-y-hidden">
+        <div className="sticky top-0 bg-[#18191a] py-3 px-2">
+          <div className="relative flex items-center">
+            <div className="absolute left-3">
+              <BiSearch className="w-5 h-5 text-gray-400" />
+            </div>
+            <input
+              type="text"
+              className={classNames(
+                'py-2 px-2 block w-full rounded-full bg-[#3a3b3c] focus:outline-none',
+                'border-none pl-10 focus:ring-0 placeholder-gray-400',
+                'focus:placeholder-gray-300'
+              )}
+              placeholder="Search"
+            />
+          </div>
+        </div>
+        <nav className="divide-y divide-gray-700 space-y-4">
+          <div>
+            <h1 className="text-xs text-gray-300 px-4 py-2">
+              Suggested Accounts
+            </h1>
+            <ul className="mt-1">
+              {dummyAccounts?.map(
+                (accounts, i) =>
+                  i <= 4 && <SuggestedAccountList key={i} {...accounts} />
+              )}
+            </ul>
+          </div>
+          <div className="py-3">
+            <h1 className="text-xs text-gray-300 px-4 py-2">
+              Following accounts
+            </h1>
+            <ul className="mt-1">
+              {dummyAccounts?.map(
+                (accounts, i) =>
+                  i <= 3 && <SuggestedAccountList key={i} {...accounts} />
+              )}
+            </ul>
+          </div>
+        </nav>
+      </section>
     </React.Fragment>
   )
 }
@@ -42,7 +91,7 @@ const SuggestedList = ({ name, Icon }: SuggedtedProps) => {
       <a
         href="#"
         className={classNames(
-          'flex items-center space-x-4 text-sm px-4 py-3 rounded-lg',
+          'flex items-center space-x-4 text-sm px-3 py-3 rounded-lg',
           'active:scale-95 transition ease-in-out duration-150',
           'hover:bg-[#303031] -mx-1'
         )}
@@ -50,6 +99,42 @@ const SuggestedList = ({ name, Icon }: SuggedtedProps) => {
         <Icon className="w-5 h-5" />
         <span>{name}</span>
       </a>
+    </li>
+  )
+}
+
+const SuggestedAccountList = ({
+  username,
+  displayName,
+  avatarUrl,
+}: DummyAccountProps) => {
+  return (
+    <li className="px-2">
+      <button
+        type="button"
+        className={classNames(
+          'w-full rounded-lg flex items-center py-1.5 px-3 space-x-4 hover:bg-[#303031]',
+          'transition ease-in-out duration-150 active:scale-95'
+        )}
+      >
+        <div className="relative w-8 h-8 rounded-full overflow-hidden">
+          <Image
+            src={avatarUrl ? avatarUrl : ''}
+            layout="fill"
+            className=""
+            alt="profile"
+          />
+        </div>
+        <div className="flex flex-col">
+          <div className="flex flex-row items-center space-x-1">
+            <h1 className="text-sm font-bold lowercase">{username}</h1>
+            <MdVerified className="w-4 h-4 text-[#20d5ec]" />
+          </div>
+          <span className="text-xs font-light text-left capitalize">
+            {displayName}
+          </span>
+        </div>
+      </button>
     </li>
   )
 }
--- src/utils/object-items.tsx
+++ src/utils/object-items.tsx
@@ -37,3 +37,69 @@ export const suggedtedItems = [
     Icon: HiOutlineCalendar,
   },
 ]
+
+export type DummyAccountProps = {
+  id: number
+  username: string
+  displayName: string
+  avatarUrl?: string
+}
+
+export const dummyAccounts: DummyAccountProps[] = [
+  {
+    id: 1,
+    username: 'nianaguerrero',
+    displayName: 'Niana Guerero',
+    avatarUrl:
+      'https://p16-sign-va.tiktokcdn.com/musically-maliva-obj/1653642385041413~c5_100x100.jpeg?x-expires=1654246800&x-signature=6DKBLNfUz9EmEEr6uYgJA3B007g%3D',
+  },
+  {
+    id: 2,
+    username: 'rosmartan',
+    displayName: 'Mommy Rosmar',
+    avatarUrl:
+      'https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/6bc6a635aa488a892d1a33aeb1c954e0~c5_100x100.jpeg?x-expires=1654246800&x-signature=w6iC1A4TnWUj9UndebunsRlAXII%3D',
+  },
+  {
+    id: 3,
+    username: 'yanyandejesus1',
+    displayName: 'Yanyan De Jesus',
+    avatarUrl:
+      'https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tiktok-obj/1645999420289026.jpeg?x-expires=1654246800&x-signature=anI1h0XoH173O1a6pzDbUCfA2BY%3D',
+  },
+  {
+    id: 4,
+    username: 'callmegrand',
+    displayName: 'Vladimir Grand',
+    avatarUrl:
+      'https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/cc52ea21fbc28d4558cba91f85275ebc~c5_100x100.jpeg?x-expires=1654246800&x-signature=51axMDp0a9ZQ81hPJZ3v6yV9QHk%3D',
+  },
+  {
+    id: 5,
+    username: 'eyevanana',
+    displayName: 'Eya',
+    avatarUrl:
+      'https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/77beb47f005fcff2a108e426725f77b1~c5_100x100.jpeg?x-expires=1654246800&x-signature=y1VLSbVO8FQXSeq%2B3suh%2Bc3qldI%3D',
+  },
+  {
+    id: 6,
+    username: 'itsevantan',
+    displayName: 'Evan Tan',
+    avatarUrl:
+      'https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/9734757566c37e2017f1b09dac3d696e~c5_100x100.jpeg?x-expires=1654246800&x-signature=ctZsuzYuEKw4n0wnzWO%2BebHFN5c%3D',
+  },
+  {
+    id: 7,
+    username: 'siliqueenonfficial',
+    displayName: 'siliqueen',
+    avatarUrl:
+      'https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/00149023a5266dda456466918c10fd8a.jpeg?x-expires=1654246800&x-signature=O4Bx%2F7IvHLeTms7SCkmk90BaF%2Bg%3D',
+  },
+  {
+    id: 8,
+    username: 'spencer_serafica',
+    displayName: 'siliqueen',
+    avatarUrl:
+      'https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/28ea3987da71d761d44482d7218c28f8~c5_100x100.jpeg?x-expires=1654246800&x-signature=kUqBvV5pEjDE7sR1YEUtEgKJt2k%3D',
+  },
+]
