454e9fc6b2ab2b7f2b17f0e5f41cc13c8c5445c8
acatzk <joshuaimalay@gmail.com>
2022-06-01 14:39:39 +0800

    feat: add logout menu toggle (#23)

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Image from 'next/image'
 import { FaSearch } from 'react-icons/fa'
 import { useUserData } from '@nhost/react'
-import { BsMessenger } from 'react-icons/bs'
+import { BsMessenger, BsCheck2 } from 'react-icons/bs'
 import { classNames } from '~/utils/classNames'
+import { Popover, Transition } from '@headlessui/react'
 import { BiDotsHorizontalRounded } from 'react-icons/bi'
 import { RiNotification3Fill, RiHome7Fill } from 'react-icons/ri'
 
-type Props = {}
+type Props = {
+  actions: any
+}
 
 const SideBar: React.FC<Props> = (props) => {
   const user = useUserData()
+  const { handleLogout } = props?.actions
 
   return (
     <aside className="bg-[#242526] border-r border-gray-700">
@@ -29,13 +33,13 @@ const SideBar: React.FC<Props> = (props) => {
             />
           </div>
         </button>
-        <ul className="flex flex-col items-center space-y-2">
+        <ul className="flex flex-col items-center space-y-2 mb-3">
           <li className="flex items-center">
             <div className="w-1 h-9 bg-indigo-500 rounded-r"></div>
             <button
               className={classNames(
                 'text-indigo-500 rounded-lg hover:bg-[#3a3b3c] p-3 mx-3',
-                'transition ease-in-out duration-150'
+                'transition ease-in-out duration-150 active:scale-95'
               )}
             >
               <RiHome7Fill className="w-7 h-7" />
@@ -46,7 +50,7 @@ const SideBar: React.FC<Props> = (props) => {
             <button
               className={classNames(
                 'text-[#e7e9ec] rounded-lg hover:bg-[#3a3b3c] p-4 mx-3',
-                'transition ease-in-out duration-150'
+                'transition ease-in-out duration-150 active:scale-95'
               )}
             >
               <FaSearch className="w-5 h-5" />
@@ -57,7 +61,7 @@ const SideBar: React.FC<Props> = (props) => {
             <button
               className={classNames(
                 'text-[#e7e9ec] rounded-lg hover:bg-[#3a3b3c] p-3.5 mx-3',
-                'transition ease-in-out duration-150'
+                'transition ease-in-out duration-150 active:scale-95'
               )}
             >
               <RiNotification3Fill className="w-6 h-6" />
@@ -68,7 +72,7 @@ const SideBar: React.FC<Props> = (props) => {
             <button
               className={classNames(
                 'text-[#e7e9ec] rounded-lg hover:bg-[#3a3b3c] p-3.5 mx-3',
-                'transition ease-in-out duration-150'
+                'transition ease-in-out duration-150 active:scale-95'
               )}
             >
               <BsMessenger className="w-6 h-6" />
@@ -76,14 +80,72 @@ const SideBar: React.FC<Props> = (props) => {
           </li>
         </ul>
         <div className="mb-3">
-          <button
-            className={classNames(
-              'rounded-full hover:bg-indigo-500 p-2',
-              'transition ease-in-out duration-150'
+          <Popover className="relative">
+            {({ open }) => (
+              <>
+                <Popover.Button
+                  className={classNames(
+                    'rounded-full hover:bg-indigo-500 p-2',
+                    'transition ease-in-out duration-150 active:scale-95',
+                    'focus:outline-none'
+                  )}
+                >
+                  <BiDotsHorizontalRounded className="w-8 h-8" />
+                </Popover.Button>
+                <Transition
+                  as={Fragment}
+                  enter="transition ease-out duration-200"
+                  enterFrom="opacity-0 translate-y-1"
+                  enterTo="opacity-100 translate-y-0"
+                  leave="transition ease-in duration-150"
+                  leaveFrom="opacity-100 translate-y-0"
+                  leaveTo="opacity-0 translate-y-1"
+                >
+                  <Popover.Panel className="absolute -top-36 left-40 z-10 w-screen max-w-xs transform -translate-x-1/2 ">
+                    <div className="absolute bg-[#18191a] w-5 h-5 -bottom-1 rotate-45 left-4 shadow-md -z-10"></div>
+                    <div className="bg-[#18191a] overflow-hidden rounded-lg shadow-md ring-1 ring-white ring-opacity-5 z-20">
+                      <div className="flex py-4 justify-between">
+                        <div className="flex items-center px-4 space-x-4">
+                          <div className="relative w-10 h-10 rounded-full overflow-hidden">
+                            <Image
+                              src={
+                                user
+                                  ? user?.avatarUrl
+                                  : 'https://miro.medium.com/max/720/1*W35QUSvGpcLuxPo3SRTH4w.png'
+                              }
+                              layout="fill"
+                              className=""
+                              alt="profile"
+                            />
+                          </div>
+                          <div className="flex flex-col">
+                            <h1 className="uppercase text-sm font-bold">
+                              {user?.displayName}
+                            </h1>
+                            <span className="text-xs font-light">
+                              {user?.email}
+                            </span>
+                          </div>
+                        </div>
+                        <BsCheck2 className="w-6 h-6 text-indigo-500 mr-4" />
+                      </div>
+                      <button
+                        type="button"
+                        onClick={handleLogout}
+                        className={classNames(
+                          'border-t border-gray-800 py-4 px-4 text-sm w-full text-left',
+                          'hover:bg-[#262729] transition ease-in-out duration-150',
+                          'active:bg-[#18191a]'
+                        )}
+                      >
+                        Log out @angryboy_19
+                      </button>
+                    </div>
+                  </Popover.Panel>
+                </Transition>
+              </>
             )}
-          >
-            <BiDotsHorizontalRounded className="w-8 h-8" />
-          </button>
+          </Popover>
         </div>
       </nav>
     </aside>
--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,23 +1,46 @@
 import React from 'react'
 import Head from 'next/head'
 import SideBar from '~/components/SideBar'
+import { nhost } from '~/lib/nhost-client'
+import { getNhostSession } from '@nhost/nextjs'
+import { GetServerSidePropsContext } from 'next'
 
 type Props = {
   metaHead?: string
   children: React.ReactNode
 }
 
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const nhostSession = await getNhostSession(
+    `${process.env.NEXT_PUBLIC_NHOST_BACKEND}`,
+    context
+  )
+
+  return {
+    props: {
+      nhostSession,
+    },
+  }
+}
+
 const Layout: React.FC<Props> = (props) => {
-  const { metaHead } = props
+  const { metaHead, children } = props
+
+  const handleLogout = async () => await nhost.auth.signOut()
+
   return (
     <div className="antialiased bg-[#18191a] text-[#e7e9ec] min-h-screen">
       <Head>
         <title>{metaHead} / Gewgew</title>
       </Head>
       <div className="flex flex-row min-h-screen">
-        <SideBar />
+        <SideBar actions={{ handleLogout }} />
         <div className="relative w-1/4 flex-grow hidden lg:block border-r border-gray-700"></div>
-        <div className="relative w-1/2 flex-grow lg:flex-none border-r border-gray-700"></div>
+        <div className="relative w-1/2 flex-grow lg:flex-none border-r border-gray-700">
+          {children}
+        </div>
         <div className="relative w-1/4   flex-grow hidden md:block"></div>
       </div>
     </div>
